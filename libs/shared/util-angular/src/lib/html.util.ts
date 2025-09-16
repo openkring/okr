c7bb55e4cb97f6e299f2bc5236a90e0f4b3c12ec
Bruno Kaiser <bruno@bkaiser.ch>
2025-09-16 22:11:47 +0200

    removing problematic platform check - util-angular is implicitly always client

--- libs/shared/util-angular/src/lib/html.util.ts
+++ libs/shared/util-angular/src/lib/html.util.ts
@@ -1,14 +1,8 @@
-import { PLATFORM_ID, inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-
-
 /**
    * source: https://github.com/ionic-team/ionic-framework/issues/18692
    * @returns true, if we are within a splitpane
    */
 export function isInSplitPane(): boolean {
-  const platformId = inject(PLATFORM_ID);
-  if (!isPlatformBrowser(platformId)) return false;
   const _splitPane = document.querySelector('ion-split-pane');
   if (_splitPane === null || _splitPane === undefined) return false;
   return window.innerWidth >= 992;
