340689f83a04fa9efbecde8eb3e42e446e1ac042
Bruno Kaiser <bruno@bkaiser.ch>
2026-03-05 21:34:01 +0100

    better getDefaultIcon in AvatarPipe based on the given modeltype

--- libs/avatar/ui/src/lib/avatar.pipe.ts
+++ libs/avatar/ui/src/lib/avatar.pipe.ts
@@ -11,6 +11,18 @@ export class AvatarPipe implements PipeTransform {
   private readonly avatarService = inject(AvatarService);
 
   transform(key: string, defaultIcon?: string): string {
-    return this.avatarService.getAvatarUrl(key, defaultIcon ?? 'other');
+    return this.avatarService.getAvatarUrl(key, defaultIcon ?? this.getDefaultIcon(key));
+  }
+
+  private getDefaultIcon(key: string): string {
+    const [modelType, bkey] = key.split('.');
+    switch (modelType) {
+      case 'person':
+      case 'org':
+      case 'resource':
+        return modelType;
+      default:
+        return 'other';
+    }
   }
 }
