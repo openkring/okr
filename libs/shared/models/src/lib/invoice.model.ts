5089c9c2f27d2b26c30636a2e305edb37e2fb110
Bruno Kaiser <bruno@bkaiser.ch>
2026-04-01 15:24:23 +0200

    using AvatarInfo in InvoiceModel

--- libs/shared/models/src/lib/invoice.model.ts
+++ libs/shared/models/src/lib/invoice.model.ts
@@ -1,4 +1,6 @@
-import { DEFAULT_CURRENCY, DEFAULT_DATE, DEFAULT_GENDER, DEFAULT_ID, DEFAULT_INDEX, DEFAULT_KEY, DEFAULT_NAME, DEFAULT_NOTES, DEFAULT_ORG_TYPE, DEFAULT_PRICE, DEFAULT_TAGS, DEFAULT_TENANTS, DEFAULT_TITLE, DEFAULT_URL } from '@bk2/shared-constants';
+import { DEFAULT_DATE, DEFAULT_ID, DEFAULT_INDEX, DEFAULT_KEY, DEFAULT_NOTES, DEFAULT_TAGS, DEFAULT_TENANTS, DEFAULT_TITLE, DEFAULT_URL } from '@bk2/shared-constants';
+import { AvatarInfo, MoneyModel } from '@bk2/shared-models';
+
 import { BkModel, SearchableModel, TaggedModel } from './base.model';
 import { InvoicePositionModel } from './invoice-position.model';
 
@@ -11,7 +13,7 @@ export class InvoiceModel implements BkModel, SearchableModel, TaggedModel {
   public isArchived = false;
   public index = DEFAULT_INDEX;
   public tags = DEFAULT_TAGS;
-  public description = DEFAULT_NOTES; // a detailed description of the trip
+  public notes = DEFAULT_NOTES; // a detailed description of the invoice
 
   public title = DEFAULT_TITLE;
   public invoiceId = DEFAULT_ID; // Rechnungsnummer
@@ -21,28 +23,19 @@ export class InvoiceModel implements BkModel, SearchableModel, TaggedModel {
   public footer = ''; // Rechnungsfuss
   public invoicePositions: InvoicePositionModel[] = [];
 
-  public amount = DEFAULT_PRICE; // total amount
-  public currency = DEFAULT_CURRENCY;
+  public totalAmount: MoneyModel | undefined;
   public taxes = 0; // total taxes
   public taxRate = 0; // tax rate in percent
-  public isPaid = false; // is the invoice paid
+  public state = 'created'; // Category invoice_state
   public paymentDate = DEFAULT_DATE; // Datum der Zahlung
   public bexioUrl = DEFAULT_URL; // URL to bexio invoice
   // booking-account is on the invoice position
 
-  // subject = invoice sender (Person or Org) Rechnungssteller
-  public subjectKey = DEFAULT_KEY;
-  public subjectName = DEFAULT_NAME;
-  public subjectModelType: 'person' | 'org' = 'person';
-  public subjectType = DEFAULT_GENDER;
-  public subjectBexioId = DEFAULT_ID; // Bexio ID of the subject
+  // invoice sender (Person or Org) Rechnungssteller
+  public invoiceSender: AvatarInfo | undefined;   // read bexioid from person
 
-  // object = invoice receiver (Person or Org) Rechnungsempfänger
-  public objectKey = DEFAULT_KEY;
-  public objectName = DEFAULT_NAME;
-  public objectModelType: 'person' | 'org' = 'org';
-  public objectType = DEFAULT_ORG_TYPE;
-  public objectBexioId = DEFAULT_ID; // Bexio ID of the object
+  // invoice receiver (Person or Org) Rechnungsempfänger
+  public invoiceReceiver: AvatarInfo | undefined;
 
   constructor(tenantId: string) {
     this.tenants = [tenantId];
