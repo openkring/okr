ac0d52f9279e0f45f068c711cf1c7130edcc0fd3
Bruno Kaiser <bruno@bkaiser.ch>
2026-05-04 17:13:29 +0200

    fix(flighttracker): guard planeIconUrl against non-finite direction, fix colour encoding

--- libs/geo/flighttracker/feature/src/lib/flighttracker-search.component.ts
+++ libs/geo/flighttracker/feature/src/lib/flighttracker-search.component.ts
@@ -19,10 +19,11 @@ import { FlightDetailModal } from './flighttracker-detail.modal';
 import { FlightTrackerStore } from './flighttracker.store';
 
 function planeIconUrl(direction: number): string {
+  const deg = Number.isFinite(direction) ? direction : 0;
   const svg =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">' +
-    `<g transform="rotate(${direction}, 12, 12)">` +
-    '<path fill="%233880ff" d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/>' +
+    `<g transform="rotate(${deg}, 12, 12)">` +
+    '<path fill="#3880ff" d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/>' +
     '</g></svg>';
   return 'data:image/svg+xml;utf8,' + encodeURIComponent(svg);
 }
