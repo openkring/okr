c2d81271107fbc391f272e9b7d2b8db1e0f12024
Bruno Kaiser <bruno@bkaiser.ch>
2026-05-30 14:13:09 +0200

    refactor(cms-section): use bare 'required' code in responsibility validation

--- libs/cms/section/ui/src/lib/responsibility-section.validations.ts
+++ libs/cms/section/ui/src/lib/responsibility-section.validations.ts
@@ -3,7 +3,7 @@ import { create, enforce, test } from 'vest';
 import { ResponsibilityConfig } from '@bk2/shared-models';
 
 export const responsibilitySectionValidations = create((data: ResponsibilityConfig) => {
-  test('bkey', 'validation.required', () => {
+  test('bkey', 'required', () => {
     enforce(data.bkey).isNotEmpty();
   });
 });
