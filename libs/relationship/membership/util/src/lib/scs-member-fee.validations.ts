39ca0b10eb815a6f7dbb1bdc33d48084379b6a96
Bruno Kaiser <bruno@bkaiser.ch>
2026-05-30 14:12:02 +0200

    refactor(membership): replace English validation codes with generic bare codes

--- libs/relationship/membership/util/src/lib/scs-member-fee.validations.ts
+++ libs/relationship/membership/util/src/lib/scs-member-fee.validations.ts
@@ -8,16 +8,16 @@ export const scsMemberFeeValidations = staticSuite((model: ScsMemberFeesModel, t
 
   baseValidations(model, tenants, tags, field);
 
-  test('jb', 'jb must be >= 0', () => { enforce(model.jb).greaterThanOrEquals(0); });
-  test('srv', 'srv must be >= 0', () => { enforce(model.srv).greaterThanOrEquals(0); });
-  test('bev', 'bev must be >= 0', () => { enforce(model.bev).greaterThanOrEquals(0); });
-  test('entryFee', 'entryFee must be >= 0', () => { enforce(model.entryFee).greaterThanOrEquals(0); });
-  test('locker', 'locker must be >= 0', () => { enforce(model.locker).greaterThanOrEquals(0); });
-  test('hallenTraining', 'hallenTraining must be >= 0', () => { enforce(model.hallenTraining).greaterThanOrEquals(0); });
-  test('skiff', 'skiff must be >= 0', () => { enforce(model.skiff).greaterThanOrEquals(0); });
-  test('skiffInsurance', 'skiffInsurance must be >= 0', () => { enforce(model.skiffInsurance).greaterThanOrEquals(0); });
+  test('jb', 'tooSmall', () => { enforce(model.jb).greaterThanOrEquals(0); });
+  test('srv', 'tooSmall', () => { enforce(model.srv).greaterThanOrEquals(0); });
+  test('bev', 'tooSmall', () => { enforce(model.bev).greaterThanOrEquals(0); });
+  test('entryFee', 'tooSmall', () => { enforce(model.entryFee).greaterThanOrEquals(0); });
+  test('locker', 'tooSmall', () => { enforce(model.locker).greaterThanOrEquals(0); });
+  test('hallenTraining', 'tooSmall', () => { enforce(model.hallenTraining).greaterThanOrEquals(0); });
+  test('skiff', 'tooSmall', () => { enforce(model.skiff).greaterThanOrEquals(0); });
+  test('skiffInsurance', 'tooSmall', () => { enforce(model.skiffInsurance).greaterThanOrEquals(0); });
 
   numberValidations('rebate', model.rebate, false, 0);
 
-  test('state', 'state is required', () => { enforce(model.state).isNotEmpty(); });
+  test('state', 'required', () => { enforce(model.state).isNotEmpty(); });
 });
